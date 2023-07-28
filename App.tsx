438a1d3b54b7d566f23d596276370b0e6a5728b3
eniyavan <eniyavankumar7979@gmail.com>
2023-07-28 12:03:58 +0530

    profile v1

--- App.tsx
+++ App.tsx
@@ -1,10 +1,20 @@
 import { NavigationContainer } from '@react-navigation/native';
 import * as React from 'react';
 import Navigator from './routes/HomeStack'
+import SplashScreen from 'react-native-splash-screen';
+import { View } from 'react-native';
+import { useEffect } from 'react';
 
 const App = () => {
+  
+  useEffect(() =>{
+    SplashScreen.hide();
+  },[]);
   return (
-   <Navigator/>
+    
+    <Navigator/>
+    
+   
   );
 };
 
--- styles/HomeStyles.tsx
+++ styles/HomeStyles.tsx
@@ -1,13 +1,42 @@
 import {StyleSheet} from 'react-native'
+import { COLORS, FONT } from '../constants/Theme';
 
 
 
-const   ProfileStyles = StyleSheet.create({
+const   Styless = StyleSheet.create({
     container : {
         flex: 1,
         justifyContent: 'center',
         alignItems: 'center',
     },
+ txt:{
+             color:COLORS.primary,
+             fontWeight:`900`,
+             fontSize:40,
+             alignContent:'center',
+             marginLeft:90,
+             marginTop:250
+             
+    },
+    btncontainer:{
+        elevation: 8,
+          backgroundColor:COLORS.primary,
+          borderRadius: 10,
+          paddingVertical: 10,
+          paddingHorizontal: 12,
+          justifyContent:"space-between",
+          margin:5,
+          alignContent:"center",
+          marginTop:250
+          
+      },
+      btntxt:{
+        fontSize: 18,
+        color: "#fff",
+        fontWeight: "bold",
+        alignSelf: "center",
+        textTransform: "uppercase"
+      },
     title : {
         color : "#000"
     }
@@ -15,4 +44,4 @@ const   ProfileStyles = StyleSheet.create({
 })
 
 
-export default ProfileStyles
+export default Styless
