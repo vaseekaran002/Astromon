c124b2d3c8fa6a0e01135ce93f01f53e23d03dab
eniyavan <eniyavankumar7979@gmail.com>
2023-08-16 10:58:08 +0530

    edit button fix

--- screens/Home.tsx
+++ screens/Home.tsx
@@ -1,30 +1,65 @@
-import React from 'react'
-import { View ,Text ,TouchableOpacity} from 'react-native'
+import React, { useState, useEffect } from 'react'
+import { View, Text } from 'react-native'
 import { SafeAreaView } from 'react-native-safe-area-context'
-import HomeStyles from '../styles/HomeStyles'
-import Styless from '../styles/HomeStyles'
-import Profile from './Profile'
 import AsyncStorage from '@react-native-async-storage/async-storage';
+import Styless from '../styles/HomeStyles';
 
+interface FormValues {
+  name: string;
+  age: string;
+  deviceid: string;
+  bloodgroup: string;
+  gender: string;
+  height: string;
+  weight: string;
+  
+}
+const Home = () => {
+  const initialValues: FormValues = {
+    name: '',
+    age: '',
+    deviceid: '',
+    bloodgroup: '',
+    gender: '',
+    height: '',
+    weight: '',
+   
 
+  };
+ 
+  const [userData, setUserData] = useState(initialValues);
+  const [isLoading, setIsLoading] = useState(true);
 
-const Home = ( ) => {
-  // const getUser = async () => {
-  //   try {
-  //     const savedUser = await AsyncStorage.getItem("user");
-  //     const currentUser = JSON.parse(savedUser);
-  //     console.log(currentUser);
-  //   } catch (error) {
-  //     console.log(error);
-  //   }
-  // };
+  useEffect(() => {
+    getUserData();
+  }, []);
+
+  const getUserData = async () => {
+    try {
+      const savedUser = await AsyncStorage.getItem("userProfile") || '{}';
+      const currentUser = JSON.parse(savedUser);
+      setUserData(currentUser);
+      setIsLoading(false);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <SafeAreaView>
-        <View >
-            <Text style={Styless.txt} > ASTROMON  </Text>
-        </View>
+      <View>
+        <Text style={Styless.txt}> ASTROMON </Text>
+        {isLoading ? (
+          <Text>Loading...</Text>
+        ) : (
+         
+          <Text style={Styless.txt}>Welcome ,{userData.name}</Text>
+           
+
+        )}
+      </View>
     </SafeAreaView>
-  )
+  );
 }
 
-export default Home
+export default Home;
